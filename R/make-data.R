37f97f97707f2b4203f4aaffc1457e68fa695918
Andreas Beger <adbeger@gmail.com>
2018-11-12 11:09:23 +0200

    Update question dump to add more questions with resolution, close #2

--- R/make-data.R
+++ R/make-data.R
@@ -9,7 +9,7 @@ dropbox_path  <- "~/Dropbox/Work/asia-polmeth-2019"
 in_dir  <- file.path(dropbox_path, "input-data")
 out_dir <- file.path(dropbox_path, "output")
 user_dump     <- "dump_user_forecasts_09_07_2018_09_15_12.csv"
-question_dump <- "dump_questions_09_07_2018_09_30_55.csv" 
+question_dump <- "dump_questions_11_12_2018_08_51_25.csv" 
 
 
 mbrier <- function(f, o, ordered = TRUE) {
@@ -35,13 +35,25 @@ mbrier <- function(f, o, ordered = TRUE) {
   return(brier)
 }
 
+# The latest question dump don't have IFP_id's anymore, need to back-engineer 
+# this based on title
+ifp_id_mapping <- file.path(in_dir, "dump_questions_09_07_2018_09_30_55.csv") %>%
+  read_csv(., col_types = cols(
+    .default = col_character(),
+    ifp_id = col_integer(),
+    discover_id = col_integer(),
+    start_date = col_datetime(format = ""),
+    end_date = col_datetime(format = ""),
+    resolved_date = col_datetime(format = ""),
+    voided_date = col_datetime(format = ""),
+    num_options = col_integer()
+  )) %>%
+  select(ifp_id, title)
 
-q_dump <- file.path(dropbox_path, "input-data", question_dump) %>%
+q_dump <- file.path(in_dir, question_dump) %>%
   read_csv(.,
            col_types = cols(
              .default = col_character(),
-             ifp_id = col_integer(),
-             discover_id = col_integer(),
              start_date = col_datetime(format = ""),
              end_date = col_datetime(format = ""),
              resolved_date = col_datetime(format = ""),
@@ -49,11 +61,20 @@ q_dump <- file.path(dropbox_path, "input-data", question_dump) %>%
              num_options = col_integer(),
              is_ordinal = col_logical(),
              is_resolved = col_logical(),
-             is_voided = col_logical(),
-             has_chart = col_logical()
+             is_voided = col_logical()
            )) %>%
   mutate_at(vars(contains("date")), as.Date) %>%
-  mutate_at(vars(starts_with("has"), starts_with("is")), ~.x=="True")
+  mutate_at(vars(starts_with("has"), starts_with("is")), ~.x=="True") %>%
+  # add in IFP ID
+  full_join(ifp_id_mapping, by = "title")
+
+# only 2 practice questions
+no_q  <- filter(q_dump, !is.na(ifp_id) & is.na(question_id))
+# since no unmatched IFP IDs, should be all good
+no_id <- filter(q_dump, is.na(ifp_id))
+
+q_dump <- q_dump %>%
+  filter(!is.na(ifp_id))
 
 # Questions that require data aggregation
 agg_q <- q_dump %>%
@@ -121,8 +142,8 @@ user_w_answer <- left_join(user_clean, answers, by = "ifp_id") %>%
          # don't need these
          -discover_id, -rationale)
 flog.info("%s%% of %s forecasts are dropping out because no resolution is available",
-          (sum(is.na(user$correct_option))/nrow(user)*100) %>% round(1),
-          format(nrow(user), big.mark = ","))
+          (sum(is.na(user_w_answer$correct_option))/nrow(user_w_answer)*100) %>% round(1),
+          format(nrow(user_w_answer), big.mark = ","))
 # Add in Brier and condition info
 user_w_brier <- user_w_answer %>%
   filter(!is.na(correct_option)) %>%
@@ -136,7 +157,7 @@ user_w_brier <- user_w_answer %>%
   spread(option, p) 
   
 # Add in condition indicators
-user_w_cond <- user_w_brier %>%
+user_w_features <- user_w_brier %>%
   mutate(sees_model = has_arima*(condition=="c"),
          sees_chart = has_arima*(condition %in% c("b", "c")),
          needs_data_agg = ifp_id %in% agg_q)
@@ -144,13 +165,13 @@ user_w_cond <- user_w_brier %>%
 flog.info("Clean user forecasts: N = %s",
           format(nrow(user_clean), big.mark = ","))
 flog.info("User forecasts with answer: N = %s",
-          format(nrow(user_w_cond), big.mark = ","))
+          format(nrow(user_w_features), big.mark = ","))
 
 write_rds(user_clean, file.path(out_dir, "user_forecasts_clean.rds"))
 write_csv(user_clean, file.path(out_dir, "user_forecasts_clean.csv"))
 
-write_rds(user_w_cond, file.path(out_dir, "user_forecasts_w_features.rds"))
-write_csv(user_w_cond, file.path(out_dir, "user_forecasts_w_features.csv"))
+write_rds(user_w_features, file.path(out_dir, "user_forecasts_w_features.rds"))
+write_csv(user_w_features, file.path(out_dir, "user_forecasts_w_features.csv"))
 
 
 
