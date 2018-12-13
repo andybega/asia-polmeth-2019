54ff259503a9a8148ded1114ff237a957b5bbdaa
Andreas Beger <adbeger@gmail.com>
2018-12-13 10:57:01 +0200

    clean up figure and start slides

--- R/analysis.R
+++ R/analysis.R
@@ -292,6 +292,8 @@ mdl3_coefs <- tidy(mdl3) %>%
   mutate(Forecaster = gsub("Forecaster", "", Forecaster),
          `Data source` = gsub("Data_source", "", `Data source`)) %>%
   mutate(`Data source` = factor(`Data source`))
+# Reorder by median estimate; makes plot easier to read
+mdl3_coefs$`Data source` = fct_reorder(mdl3_coefs$`Data source`, mdl3_coefs$estimate)
 
 mdl3_coefs %>%
   mutate(facet = ifelse(Forecaster=="Machine (reference)", "Baseline", "Relative difference")) %>%
@@ -476,56 +478,9 @@ ggplot(fcasts_good_machine, aes(x = interaction(Forecaster, Condition), y = brie
 
 
 
-## older stuff, organize
 
-# activity by question
-fcasts_by_ifp <- all_fcasts %>%
-  group_by(ifp_id) %>%
-  summarize(avg_Brier = mean(brier), n = n()) %>%
-  arrange(desc(n))
-fcasts_by_ifp
-tail(fcasts_by_ifp)
+# Selection effects -------------------------------------------------------
 
-# 2126 had a lot of forecasts
-f2126 <- filter(all_fcasts, ifp_id==2126)
-table(f2126$Forecaster, f2126$condition)
-f2126 %>% 
-  group_by(Forecaster, condition) %>%
-  summarize(avg_Brier = mean(brier), n = n())
-
-# 2297
-f2297 <- filter(all_fcasts, ifp_id==2297)
-table(f2297$Forecaster, f2297$condition)
-f2297 %>% 
-  group_by(Forecaster, condition) %>%
-  summarize(avg_Brier = mean(brier), n = n())
-
-# low activity 
-# 1208
-f1208 <- filter(all_fcasts, ifp_id==1208)
-table(f1208$Forecaster, f1208$condition)
-f1208 %>% 
-  group_by(Forecaster, condition) %>%
-  summarize(avg_Brier = mean(brier), n = n())
-
-# 1235
-f1235 <- filter(all_fcasts, ifp_id==1235)
-table(f1235$Forecaster, f1235$condition)
-f1235 %>% 
-  group_by(Forecaster, condition) %>%
-  summarize(avg_Brier = mean(brier), n = n())
-
-# 1172
-f1172 <- filter(all_fcasts, ifp_id==1172)
-f1172 %>% 
-  group_by(Forecaster, condition) %>%
-  summarize(avg_Brier = mean(brier), n = n())
-
-# 1892
-f1892 <- filter(all_fcasts, ifp_id==1892)
-f1892 %>% 
-  group_by(Forecaster, condition) %>%
-  summarize(avg_Brier = mean(brier), n = n())
 
 
 # Look at groupings by data source
@@ -590,42 +545,6 @@ foo %>%
 # users be impacted by platform design choices?
 
 
-# Compare human and machine forecasts -------------------------------------
-
-ggplot(all_fcasts, aes(x = date, y = brier)) +
-  geom_point(alpha = .1) +
-  geom_smooth(aes(color = Forecaster), se = FALSE)
-
-ggplot(all_fcasts, aes(x = date, y = brier)) +
-  geom_point(alpha = .1) +
-  geom_smooth(aes(color = condition), se = FALSE)
-
-ggplot(all_fcasts, aes(x = date, y = brier)) +
-  geom_point(alpha = .1) +
-  geom_smooth(aes(color = interaction(condition, Forecaster)), se = FALSE)
-
-
-
-
-# Summary statistics ------------------------------------------------------
-
-nrow(fcasts)
-
-# Unique IFPs
-length(unique(fcasts$ifp_id))
-
-# Forcasts by forecaster type
-table(fcasts$turker)
-
-# How many IFPs had no chart, no model?
-fcasts %>%
-  group_by(ifp_id) %>%
-  summarize(has_arima = unique(has_arima),
-            has_historic_data = unique(has_historic_data)) %>%
-  ungroup() %>%
-  summarize(ifps = n(),
-            has_arima = sum(has_arima),
-            has_historic_data = sum(has_historic_data))
 
 
 # Specific questions ------------------------------------------------------
@@ -644,188 +563,55 @@ tbl1003 <- f1003 %>%
   arrange(avg_Brier)
 tbl1003
 
-
-
-
-
-# Conditions --------------------------------------------------------------
-
-
-user_fcasts %>%
-  group_by(condition) %>% 
-  summarize(mean_brier = mean(brier))
-user_fcasts %>%
-  filter(date >= "2018-05-02") %>%
-  group_by(condition) %>% 
-  summarize(mean_brier = mean(brier))
-
-user_fcasts %>%
-  group_by(turker, sees_chart, sees_model) %>%
-  summarize(mean_brier = mean(brier))
-
-cond_tbl <- user_fcasts %>%
-  group_by(condition, turker, sees_chart, sees_model, has_historic_data) %>%
-  summarize(mean_brier = mean(brier),
-            n_ifps = length(unique(ifp_id)),
-            n_forecasts = n()) %>%
-  ungroup() %>%
-  select(condition, turker, has_historic_data, sees_chart, sees_model, everything()) %>%
-  arrange(condition, turker, has_historic_data, sees_chart, mean_brier)
-
-cond_tbl %>%
-  arrange(condition, turker, has_historic_data) %>%
-  mutate(condition = fct_recode(condition, A = "a", B = "b", C = "c"),
-         turker = fct_recode(as.character(turker), Yes = "TRUE", No = "FALSE"),
-         has_historic_data = fct_recode(as.character(has_historic_data), Yes = "TRUE", No = "FALSE")
-         ) %>%
-  rename(Condition = condition, Turker = turker, `Has data` = has_historic_data, 
-         `Saw chart` = sees_chart, `Saw model` = sees_model, 
-         `Mean Brier` = mean_brier, Questions = n_ifps, Forecasts = n_forecasts) %>%
-  write_csv("output/tables/brier-by-conditions.csv")
-
-
-# fur turker, diffs are  -0.009683274 -0.074542881  0.013981207 -0.037984003
-cond_tbl %>% 
-  arrange(condition, has_arima) %>%
-  filter(condition!="b") %>%
-  pull(mean_brier) %>%
-  diff(.) %>% 
-  `[`(seq(1, 7, by = 2))
-# for has_arima, diffs are 0.17083689 0.10597728 0.13141347 0.14777215 0.09580694
-# turkers seem to do better when they have model, little effect for humans
-cond_tbl %>% 
-  arrange(condition, turker, has_arima) %>%
-  pull(mean_brier) %>%
-  diff(.) %>% 
-  `[`(seq(1, 9, by = 2))
-# sees_chart, diffs are 0.13141347 0.14777215 0.09580694
-# however, also need to take into account has_arima difficulty
-# based on that, chart maybe helps reduce by about .17 - .13 ~ .04
-cond_tbl %>% 
-  filter(condition!="a") %>%
-  arrange(condition, turker, has_arima) %>%
-  pull(mean_brier) %>%
-  diff(.) %>% 
-  `[`(seq(1, 5, by = 2))
-# sees_model, diffs are 0.14777215 0.09580694
-# helps turkers, but hurts humans? would also be consistent with better
-# machine accuracy later in the RCT, since turkers didn't join in first few
-# weeks
-cond_tbl %>% 
-  filter(condition=="c") %>%
-  arrange(condition, turker, has_arima) %>%
-  pull(mean_brier) %>%
-  diff(.) %>% 
-  `[`(seq(1, 3, by = 2))
-
-
-# Overall, turkers actually had better (a) and no worse (c) accuracy compared
-# to humans. But this disregards IFP, etc. difference. 
-fcasts %>%
-  filter(condition!="b") %>%
-  group_by(condition, turker) %>%
-  summarize(mean_brier = mean(brier)) 
-# What if we cut off the initial period?
-fcasts %>%
-  filter(date >= "2018-05-02") %>%
-  filter(condition!="b") %>%
-  group_by(condition, turker) %>%
-  summarize(mean_brier = mean(brier)) 
-# Turkers do about .036, 0.031 worse
-
-# .168?
-fit <- lm(brier ~ turker + has_arima + sees_chart + sees_model, data = fcasts)
-predict(fit, newdata = data.frame(
-  turker = FALSE,
-  has_arima = FALSE,
-  sees_chart = 0,
-  sees_model = 0
-))
-
-df <- fcasts
-df$turker <- ifelse(df$turker==TRUE, 1, -1)
-df$has_arima <- ifelse(df$has_arima==TRUE, 1, -1)
-df$sees_chart <- ifelse(df$sees_chart==1, 1, -1)
-df$sees_model <- ifelse(df$sees_model==1, 1, -1)
-fit <- lm(brier ~ turker + has_arima + sees_chart + sees_model, data = df)
-predict(fit, newdata = data.frame(
-  turker = -1,
-  has_arima = -1,
-  sees_chart = -1,
-  sees_model = -1
-))
-
-
-fcasts %>%
-  group_by(turker, sees_chart, sees_model, has_arima) %>%
-  summarize(mean_brier = mean(brier))
-
-ggplot(fcasts, aes(x = factor(condition), y = brier)) +
-  geom_boxplot()
-
-ggplot(fcasts, aes(x = interaction(turker, sees_chart, sees_model), y = brier)) +
-  geom_boxplot()
-
-
-
-fit <- aov(brier ~ factor(turker)*factor(sees_chart)*factor(sees_model), data = fcasts)
-model.tables(fit, "means")
-
-summary(lm(brier ~ turker + has_arima + sees_chart + sees_model, data = fcasts))
-summary(lm(log(brier + 0.001) ~ turker + has_arima + sees_chart + sees_model, data = fcasts))
-
-
-# How much variation is in there in skill between users?
-user_tbl <- fcasts %>%
-  group_by(user_id) %>%
-  summarize(mean_brier = mean(brier), 
-            forecasts = n())
-hist(user_tbl$mean_brier)
-mean(user_tbl$mean_brier)
-sd(user_tbl$mean_brier)
-
-# How much, relative to how much is possible, are users forecasting?
-hist(user_tbl$forecasts)
-# add derivation of number of possible forecasts (daily)
-
-# How much variation is there in dificulty between IFPs?
-ifp_tbl <- fcasts %>%
+# activity by question
+fcasts_by_ifp <- all_fcasts %>%
   group_by(ifp_id) %>%
-  summarize(mean_brier = mean(brier), 
-            forecasts = n())
-hist(ifp_tbl$mean_brier)
-mean(ifp_tbl$mean_brier)
-sd(ifp_tbl$mean_brier)
+  summarize(avg_Brier = mean(brier), n = n()) %>%
+  arrange(desc(n))
+fcasts_by_ifp
+tail(fcasts_by_ifp)
 
-# Are users forecasting more for easy questions?
-ggplot(ifp_tbl, aes(x = mean_brier, y = forecasts)) +
-  geom_point() +
-  geom_smooth(method = "lm", se = FALSE)
-summary(lm(forecasts ~ mean_brier, data = ifp_tbl))
-# This also depends on how long a question was open, take that into account
+# 2126 had a lot of forecasts
+f2126 <- filter(all_fcasts, ifp_id==2126)
+table(f2126$Forecaster, f2126$condition)
+f2126 %>% 
+  group_by(Forecaster, condition) %>%
+  summarize(avg_Brier = mean(brier), n = n())
 
+# 2297
+f2297 <- filter(all_fcasts, ifp_id==2297)
+table(f2297$Forecaster, f2297$condition)
+f2297 %>% 
+  group_by(Forecaster, condition) %>%
+  summarize(avg_Brier = mean(brier), n = n())
 
+# low activity 
+# 1208
+f1208 <- filter(all_fcasts, ifp_id==1208)
+table(f1208$Forecaster, f1208$condition)
+f1208 %>% 
+  group_by(Forecaster, condition) %>%
+  summarize(avg_Brier = mean(brier), n = n())
 
-# Do forecasters in different conditions choose easier questions?
-df2 <- fcasts %>% 
-  group_by(ifp_id, condition, turker, date) %>%
-  summarize(has_arima = unique(has_arima), 
-            has_historic_data = unique(has_historic_data),
-            MDB = mean(brier),
-            start_date = min(date)) %>%
-  group_by(ifp_id, condition, turker) %>% 
-  summarize(has_arima = unique(has_arima), 
-            has_historic_data = unique(has_historic_data), 
-            q_cond_n = n(),
-            avg_MDB = mean(MDB),
-            start_date = min(start_date)) %>% 
-  group_by(ifp_id) %>% 
-  mutate(q_n = sum(q_cond_n),
-         start_date = min(start_date)) %>% 
-  ungroup() %>% 
-  arrange(desc(q_n))
-ggplot(df2, aes(x = factor(ifp_id), y = avg_MDB)) +
-  geom_point()
+# 1235
+f1235 <- filter(all_fcasts, ifp_id==1235)
+table(f1235$Forecaster, f1235$condition)
+f1235 %>% 
+  group_by(Forecaster, condition) %>%
+  summarize(avg_Brier = mean(brier), n = n())
+
+# 1172
+f1172 <- filter(all_fcasts, ifp_id==1172)
+f1172 %>% 
+  group_by(Forecaster, condition) %>%
+  summarize(avg_Brier = mean(brier), n = n())
+
+# 1892
+f1892 <- filter(all_fcasts, ifp_id==1892)
+f1892 %>% 
+  group_by(Forecaster, condition) %>%
+  summarize(avg_Brier = mean(brier), n = n())
+
 
 
 # Plots over time ---------------------------------------------------------
@@ -930,44 +716,6 @@ fcasts %>%
 ggsave("output/figures/volunteers-only-accuracy-by-chartability-over-time.png", height = 5, width = 8)
 
 
-# Linear model ------------------------------------------------------------
-
-fit_lm1 <- lm(brier ~ turker + has_arima + sees_chart + sees_model, data = fcasts)
-summary(fit_lm1)
-
-# Look only from when Turkers entered
-fit_lm2 <- lm(brier ~ turker + has_arima + sees_chart + sees_model, data = fcasts[fcasts$date >= "2018-05-02", ])
-summary(fit_lm2)
-
-# do with MDBs
-df <- fcasts %>%
-  filter(date >= "2018-05-02") %>%
-  group_by(ifp_id, date, turker, has_arima, sees_chart, sees_model) %>%
-  summarize(MDB = mean(brier))
-fit_lm3 <- lm(MDB ~ turker + has_arima + sees_chart + sees_model, data = df)
-summary(fit_lm3)
-
-# do with MMDBs, i.e. at IFP level
-df <- fcasts %>%
-  filter(date >= "2018-05-02") %>%
-  group_by(ifp_id, date, turker, has_arima, sees_chart, sees_model) %>%
-  summarize(MDB = mean(brier)) %>%
-  group_by(ifp_id, turker, has_arima, sees_chart, sees_model) %>%
-  summarize(MMDB = mean(MDB))
-fit_lm4 <- lm(MMDB ~ turker + has_arima + sees_chart + sees_model, data = df)
-summary(fit_lm4)
-
-
-
-summary(lm(brier ~ turker + sees_chart + sees_model + has_arima, data = fcasts[fcasts$date<"2018-06-15",]))
-summary(lm(brier ~ turker + sees_chart + sees_model + has_arima, data = fcasts[fcasts$date>="2018-06-16",]))
-summary(lm(brier ~ turker + sees_chart + sees_model + has_arima, data = fcasts[fcasts$date>="2018-07-01",]))
-summary(lm(brier ~ turker + sees_chart + sees_model + has_arima, data = fcasts[fcasts$date>="2018-07-16",]))
-summary(lm(brier ~ turker + sees_chart + sees_model + has_arima, data = fcasts[fcasts$date>="2018-08-01",]))
-summary(lm(brier ~ turker + sees_chart + sees_model + has_arima, data = fcasts[fcasts$date>="2018-08-16",]))
-summary(lm(brier ~ turker + sees_chart + sees_model + has_arima, data = fcasts[fcasts$date>="2018-09-01",]))
-
-
 
 # Beta regression ---------------------------------------------------------
 
@@ -985,26 +733,6 @@ summary(fit_beta)
 fit_beta2 <- stan_betareg(brier ~ turker + sees_chart + sees_model + has_arima,
                           data = df)
 
-
-# LMER --------------------------------------------------------------------
-
-
-fit_lmer1 <- lmer(brier ~ turker + has_arima + sees_chart + sees_model + 
-                    (1|ifp_id), data = fcasts)
-summary(fit_lmer1)
-MuMIn::r.squaredGLMM(fit_lmer1)
-
-fit_lmer2 <- lmer(brier ~ x + turker + has_arima + x*sees_chart + x*sees_model + (1|ifp_id), data = fcasts)
-summary(fit_lmer2)
-MuMIn::r.squaredGLMM(fit_lmer2)
-
-fit_lmer3 <- lmer(brier ~ x + turker + has_arima + x*sees_chart + x*sees_model + 
-                    (1|ifp_id) + (1|user_id), data = fcasts)
-summary(fit_lmer3)
-MuMIn::r.squaredGLMM(fit_lmer3)
-
-fit <- lm(brier ~ turker + has_arima + sees_chart + sees_model + factor(ifp_id), data = fcasts)
-summary(fit)
 
 
 
@@ -1098,15 +826,3 @@ user %>%
   group_by(condition, turker, has_arima) %>%
   summarize(mean(brier))
 
-
-
-summary(lm(brier ~ as.logical(sees_chart)*needs_data_agg, data = user[user$has_arima,]))
-summary(lm(brier ~ as.logical(sees_chart)*needs_data_agg + sees_model, data = user[user$has_arima,]))
-summary(lm(brier ~ as.logical(sees_chart)*needs_data_agg + sees_model + turker, data = user[user$has_arima & user$date>="2018-06-15",]))
-summary(lm(brier ~ -1 + as.logical(sees_chart):needs_data_agg, data = user[user$has_arima,]))
-# ME chart:
-#   no agg -.1
-#   agg -.1 + .12 ~ 0
-summary(lm(brier ~ -1+sees_chart*needs_data_agg + turker, data = user))
-summary(lm(brier ~ -1+sees_chart*needs_data_agg + turker + has_arima, data = user))
-summary(lm(brier ~ as.logical(sees_chart)*needs_data_agg, data = user[user$has_arima,]))
